e51c4991a62f498245d2016c9f445426d68edfe8
NicoCeresa <ceresanico@gmail.com>
2023-10-01 16:00:46 -0700

    messing around with reading in pdfs and getting dataframes out

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,12 +1,28 @@
+import re
+import tabula
+from tabula import read_pdf
 from pdfquery import PDFQuery
+from pdfminer.high_level import extract_pages, extract_text
 
-pdf = PDFQuery('example.pdf')
-pdf.load()
+tables = tabula.read_pdf('results_pdfs/Brussels_2023_results.pdf')
+print(tables)
 
-# Use CSS-like selectors to locate the elements
-text_elements = pdf.pq('LTTextLineHorizontal')
+# for page_layout in extract_pages('results_pdfs/Brussels_2023_results.pdf'):
+#     for element in page_layout:
+#         # print(element)
+#         pass
 
-# Extract the text from the elements
-text = [t.text for t in text_elements]
+# text = extract_text('results_pdfs/Brussels_2023_results.pdf')
 
-print(text)
+# pattern = re.compile(r"")
+
+# pdf = PDFQuery('results_pdfs/Brussels_2023_results.pdf')
+# pdf.load()
+
+# # Use CSS-like selectors to locate the elements
+# text_elements = pdf.pq('LTTextLineHorizontal')
+
+# # Extract the text from the elements
+# text = [t.text for t in text_elements]
+
+# print(text)
