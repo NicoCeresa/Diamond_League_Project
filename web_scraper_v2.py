483777ceae213eb7fec07647e4d13b88ac563bc3
NicoCeresa <ceresanico@gmail.com>
2023-10-09 21:15:21 -0700

    added test file folder and got csv output

--- web_scraper_v2.py
+++ web_scraper_v2.py
@@ -31,23 +31,62 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # results_panel = soup.find_all('th')
 # results_list = np.array([result.text for result in results_panel])
 
-columns = soup.find_all('th')
+columns = soup.find('th')
 
 result = soup.find_all('td', class_='right result')
 wind = soup.find_all('td', class_='right wind')
-pa = soup.find_all('td', class_='center')
-pr = soup.find_all('td', class_='center')
-Athlete = soup.find_all('td', class_='left achiever')
-Birth = soup.find_all('td', class_='center birthdate')
-Nat = soup.find_all('td', class_='center nationality')
-Race = soup.find_all('td', class_='right')
+# pa_pr_rec = soup.find_all('td', class_='center')
+# pr = soup.find_all('td', class_='center')
+athlete = soup.find_all('td', class_='left achiever')
+birth = soup.find_all('td', class_='center birthdate')
+nat = soup.find_all('td', class_='center nationality')
+race = soup.find_all('td', class_='right')
 place = soup.find_all('td', class_='center place')
 venue = soup.find_all('td', class_='venue')
 date = soup.find_all('td', class_='date')
 rs = soup.find_all('td', class_='center score')
-rec = soup.find_all('td', class_='center')
+# rec = soup.find_all('td', class_='center')
 
-# results_df = pd.DataFrame(columns=columns, data=results_list)
+result_list = [element.text for element in result]
+wind_list = [element.text for element in wind]
+athlete_list = [element.text for element in athlete]
+birth_list = [element.text for element in birth]
+nat_list = [element.text for element in nat]
+full_race_list = [element.text for element in race]
+place_list = [element.text for element in place]
+venue_list = [element.text for element in venue]
+date_list = [element.text for element in date]
+rs_list = [element.text for element in rs]
+
+race_list = []
+for idx, element in enumerate(full_race_list):
+    if idx % 3 == 2:
+        race_list.append(element)
+    else:
+        pass
+
+
+results_df = pd.DataFrame({'results':result_list,
+    'wind':wind_list, 
+    'athlete':athlete_list,
+    'birth_year':birth_list, 
+    'nationality':nat_list, 
+    'race':race_list, 
+    'place':place_list, 
+    'venue':venue_list,
+    'date':date_list, 
+    'rs':rs_list})
+
 # results_df.head()
-# results_list.to_csv(os.path.join('diamond_league/test_files', 'uncleaned_test.csv'))
-print([column.text for column in result])
+results_df.to_csv('test_files/uncleaned_test.csv')
+
+# print(len(result_list))
+# print(len(wind_list))
+# print(len(athlete_list))
+# print(len(birth_list))
+# print(len(nat_list))
+# print(len(race_list))
+# print(len(place_list))
+# print(len(venue_list))
+# print(len(date_list))
+# print(len(rs_list))
