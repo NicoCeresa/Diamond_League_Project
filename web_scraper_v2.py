62dce865a93231d7c71a15ca2679ccb0da583faa
NicoCeresa <ceresanico@gmail.com>
2023-10-09 21:29:03 -0700

    fixed type in header

--- web_scraper_v2.py
+++ web_scraper_v2.py
@@ -8,7 +8,7 @@ import numpy as np
 HEADERS = {
     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Encoding":"gzip, deflate, br",
-    "Accpet-Language":"en-US,en;q=0.9",
+    "Accept-Language":"en-US,en;q=0.9",
     "Connection":"keep-alive",
     "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.32.119 Safari/537.36",
     "Cache-Control":"max-age=0, no-cache, no-store",
