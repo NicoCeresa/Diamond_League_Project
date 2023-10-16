26978bfd32bd23bc1b1833aa6dcb6ad79d3e5282
NicoCeresa <ceresanico@gmail.com>
2023-10-16 10:49:07 -0700

    cleaned up the code some more to make it more readable. Finished the concatenated function and added functionality with the folder creating function

--- web_scraper_v2.py
+++ web_scraper_v2.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import os
 import pandas as pd
 import numpy as np
+from glob import glob
 
 
 HEADERS = {
@@ -18,12 +19,11 @@ HEADERS = {
 
 main_url = 'https://www.diamondleague.com/lists-results/statistics/'
 
-class Transform:
+class Extract:
 
     def init_folders(split_csv_name, concat_csv_name):
         if os.path.isdir(split_csv_name):
             pass
-        
         else:
             os.mkdir(split_csv_name)
 
@@ -31,6 +31,9 @@ class Transform:
             pass
         else:
             os.mkdir(concat_csv_name)
+
+        return [split_csv_name, concat_csv_name]
+
 
     def list_from_html(url, soup):
         result = soup.find_all('td', class_='right result')
@@ -65,6 +68,7 @@ class Transform:
                 pass
         return [result_list, wind_list, athlete_list, birth_list, nat_list, race_list, place_list, venue_list, date_list, rs_list]
     
+
     def partitioned_by_year_csv():
         
         years = [year for year in range(2010, datetime.now().year + 1)]
@@ -75,7 +79,7 @@ class Transform:
             response = requests.get(url, headers=HEADERS)
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            data_list = Transform.list_from_html(url, soup)
+            data_list = Extract.list_from_html(url, soup)
             
             same_length = all(len(lst) == len(data_list[0]) for lst in data_list)
 
@@ -96,14 +100,20 @@ class Transform:
                 'date':data_list[8], 
                 'rs':data_list[9]})
 
-            results_df.to_csv(f'uncleaned_csv_output/uncleaned_test_{year}.csv')
+            today = datetime.today().strftime('%m%d%Y')
+            folder_name = Extract.init_folders('uncleaned_partitioned_output', 'uncleaned_all_years_csv')[0]
+            results_df.to_csv(f'{folder_name}/uncleaned_partitioned_{year}_{today}.csv')
 
-# sorted(glob(os.path.join('uncleaned_csv_output', '*.csv')))
 
-        # def concat_partitioned_csv(path_to_folder):
-        #     return (pd.concat([pd.read_csv(input)]))
+    def concat_partitioned_csv():
+        file_paths = sorted(glob(os.path.join('uncleaned_partitioned_output', "*.csv")))
+        full_df = (pd.concat([pd.read_csv(input_file_path) for input_file_path in file_paths]))
+        today = datetime.today().strftime('%m%d%Y')
+        folder_name = Extract.init_folders('uncleaned_partitioned_output', 'uncleaned_all_years_csv')[1]
+        full_df.to_csv(f'{folder_name}/uncleaned_all_years_{today}.csv')
 
 
 if __name__ == '__main__':
-    Transform.init_folders('uncleaned_csv_output', 'all_years_csv')
-    Transform.partitioned_by_year_csv()
+    Extract.init_folders('uncleaned_partitioned_output', 'uncleaned_all_years_csv')
+    Extract.partitioned_by_year_csv()
+    Extract.concat_partitioned_csv()
