eba8993e66d5ef5cf2c7766e935fec413774f03a
NicoCeresa <ceresanico@gmail.com>
2023-10-16 10:16:54 -0700

    refactored code and made into OOP

--- web_scraper_v2.py
+++ web_scraper_v2.py
@@ -17,69 +17,75 @@ HEADERS = {
 
 main_url = 'https://www.diamondleague.com/lists-results/statistics/'
 
-# response = requests.get(url, headers=HEADERS)
-# soup = BeautifulSoup(response.content, 'html.parser')
+class Transform:
 
-years = [year for year in range(2010, datetime.now().year + 1)]
+    def list_from_html(url, soup):
+        result = soup.find_all('td', class_='right result')
+        wind = soup.find_all('td', class_='right wind')
+        # pa_pr_rec = soup.find_all('td', class_='center')
+        # pr = soup.find_all('td', class_='center')
+        athlete = soup.find_all('td', class_='left achiever')
+        birth = soup.find_all('td', class_='center birthdate')
+        nat = soup.find_all('td', class_='center nationality')
+        race = soup.find_all('td', class_='right')
+        place = soup.find_all('td', class_='center place')
+        venue = soup.find_all('td', class_='venue')
+        date = soup.find_all('td', class_='date')
+        rs = soup.find_all('td', class_='center score')
 
-# for year in years:
-# year = 2013
-for year in years: 
-    url = f'https://dl.all-athletics.com/dls/en/seasons-list/{year}/ffnnn/10229630/1073741823/detailed'
-    response = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(response.content, 'html.parser')
+        result_list = [element.text for element in result]
+        wind_list = [element.text for element in wind]
+        athlete_list = [element.text for element in athlete]
+        birth_list = [element.text for element in birth]
+        nat_list = [element.text for element in nat]
+        full_race_list = [element.text for element in race]
+        place_list = [element.text for element in place]
+        venue_list = [element.text for element in venue]
+        date_list = [element.text for element in date]
+        rs_list = [element.text for element in rs]
 
-    result = soup.find_all('td', class_='right result')
-    wind = soup.find_all('td', class_='right wind')
-    # pa_pr_rec = soup.find_all('td', class_='center')
-    # pr = soup.find_all('td', class_='center')
-    athlete = soup.find_all('td', class_='left achiever')
-    birth = soup.find_all('td', class_='center birthdate')
-    nat = soup.find_all('td', class_='center nationality')
-    race = soup.find_all('td', class_='right')
-    place = soup.find_all('td', class_='center place')
-    venue = soup.find_all('td', class_='venue')
-    date = soup.find_all('td', class_='date')
-    rs = soup.find_all('td', class_='center score')
-
-    result_list = [element.text for element in result]
-    wind_list = [element.text for element in wind]
-    athlete_list = [element.text for element in athlete]
-    birth_list = [element.text for element in birth]
-    nat_list = [element.text for element in nat]
-    full_race_list = [element.text for element in race]
-    place_list = [element.text for element in place]
-    venue_list = [element.text for element in venue]
-    date_list = [element.text for element in date]
-    rs_list = [element.text for element in rs]
-
-    race_list = []
-    for idx, element in enumerate(full_race_list):
-        if idx % 3 == 2:
-            race_list.append(element)
-        else:
-            pass
+        race_list = []
+        for idx, element in enumerate(full_race_list):
+            if idx % 3 == 2:
+                race_list.append(element)
+            else:
+                pass
+        return [result_list, wind_list, athlete_list, birth_list, nat_list, race_list, place_list, venue_list, date_list, rs_list]
     
-    data_list = [result_list, wind_list, athlete_list, birth_list, nat_list, race_list, place_list, venue_list, date_list, rs_list]
-    
-    same_length = all(len(lst) == len(data_list[0]) for lst in data_list)
+    def partitioned_by_year_csv():
+        
+        years = [year for year in range(2010, datetime.now().year + 1)]
 
-    if same_length:
-        print("All lists have the same length.")
-    else:
-        print("Lists have different lengths.")
-        break
+        for year in years: 
+            print(f"Processing {year} data")
+            url = f'https://dl.all-athletics.com/dls/en/seasons-list/{year}/ffnnn/10229630/1073741823/detailed'
+            response = requests.get(url, headers=HEADERS)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-    results_df = pd.DataFrame({'results':result_list,
-        'wind':wind_list, 
-        'athlete':athlete_list,
-        'birth_year':birth_list, 
-        'nationality':nat_list, 
-        'race':race_list, 
-        'place':place_list, 
-        'venue':venue_list,
-        'date':date_list, 
-        'rs':rs_list})
+            data_list = Transform.list_from_html(url, soup)
+            
+            same_length = all(len(lst) == len(data_list[0]) for lst in data_list)
 
-    # results_df.head()
-    results_df.to_csv(f'uncleaned_csv_output/uncleaned_test_{year}.csv')
+            if same_length:
+                print("All lists have the same length.")
+            else:
+                print("Lists have different lengths.")
+                break
+
+            results_df = pd.DataFrame({'results':data_list[0],
+                'wind':data_list[1], 
+                'athlete':data_list[2],
+                'birth_year':data_list[3], 
+                'nationality':data_list[4], 
+                'race':data_list[5], 
+                'place':data_list[6], 
+                'venue':data_list[7],
+                'date':data_list[8], 
+                'rs':data_list[9]})
+
+            # results_df.head()
+            results_df.to_csv(f'uncleaned_csv_output/uncleaned_test_{year}.csv')
+
+
+if __name__ == '__main__':
+    Transform.partitioned_by_year_csv()
