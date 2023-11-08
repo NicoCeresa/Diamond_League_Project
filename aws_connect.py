eea1441d507cdfafc22d32d7a03526f2bd48ab52
NicoCeresa <ceresanico@gmail.com>
2023-11-08 15:26:51 -0800

    added to connect to s3 so not in scraping file

--- aws_connect.py
+++ aws_connect.py
@@ -1,5 +1,27 @@
+import os
 import boto3
+from glob import glob
+from hush import aws_creds
 from web_scraper_v2 import Extract
 
-all_years = Extract.concatenate_partitioned_csv()
-partitioned = Extract.partitioned_by_year_to_csv()
+s3 = boto3.resource(
+    service_name = 's3',
+    region_name = 'us-west-1',
+    aws_access_key_id = aws_creds['AWS_access_key'], 
+    aws_secret_access_key=aws_creds['AWS_secret']
+)
+
+class Load:
+    
+    def load_partitioned_csv():
+        partitioned_folder = "uncleaned_partitioned_output"
+        partitioned_csvs = sorted(glob(os.path.join(partitioned_folder, '*.csv')))
+
+        for csv in partitioned_csvs:
+            s3.Bucket('diamond-league-project-s3').upload_file(Filename=csv, Key=csv)
+    
+            
+    def load_full_csv():
+        all_years_folder = "uncleaned_all_years_csv"
+        full_csv = os.path.join(all_years_folder, 'uncleaned_all_years.csv')
+        s3.Bucket('diamond-league-project-s3').upload_file(Filename=full_csv, Key=full_csv)
