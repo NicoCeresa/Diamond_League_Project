a38b675079338fb7f0ba65fd53c9b5d6f178362e
NicoCeresa <ceresanico@gmail.com>
2023-11-08 15:34:37 -0800

    added name==main and comments

--- aws_connect.py
+++ aws_connect.py
@@ -7,8 +7,8 @@ from web_scraper_v2 import Extract
 s3 = boto3.resource(
     service_name = 's3',
     region_name = 'us-west-1',
-    aws_access_key_id = aws_creds['AWS_access_key'], 
-    aws_secret_access_key=aws_creds['AWS_secret']
+    aws_access_key_id = aws_creds['AWS_access_key'], # create a separate file to store this sensitive info
+    aws_secret_access_key=aws_creds['AWS_secret'] # ^^^
 )
 
 class Load:
@@ -18,6 +18,8 @@ class Load:
         partitioned_csvs = sorted(glob(os.path.join(partitioned_folder, '*.csv')))
 
         for csv in partitioned_csvs:
+            # Filename is what your csv is named locally
+            # Key is what it will be named in the s3
             s3.Bucket('diamond-league-project-s3').upload_file(Filename=csv, Key=csv)
     
             
@@ -25,3 +27,8 @@ class Load:
         all_years_folder = "uncleaned_all_years_csv"
         full_csv = os.path.join(all_years_folder, 'uncleaned_all_years.csv')
         s3.Bucket('diamond-league-project-s3').upload_file(Filename=full_csv, Key=full_csv)
+        
+
+if __name__ == '__main__':
+    Load.load_partitioned_csv()
+    Load.load_full_csv()
