5ac66a6434f3f869653dfaf75433b1f37f3092d4
NicoCeresa <ceresanico@gmail.com>
2023-11-08 16:15:49 -0800

    removed HEADERS from public view

--- web_scraper_v2.py
+++ web_scraper_v2.py
@@ -3,23 +3,12 @@ import boto3
 import requests
 import numpy as np
 import pandas as pd
-from hush import aws_creds
+from hush import aws_creds, HEADERS
 from bs4 import BeautifulSoup
 from datetime import datetime as date
 
-HEADERS = {
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding":"gzip, deflate, br",
-    "Accept-Language":"en-US,en;q=0.9",
-    "Connection":"keep-alive",
-    "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.32.119 Safari/537.36",
-    "Cache-Control":"max-age=0, no-cache, no-store",
-    "Upgrade-Insecure-Requests":"1"
-}
-
 
 main_url = 'https://www.diamondleague.com/lists-results/statistics/'
-
 
 class Extract:
 
